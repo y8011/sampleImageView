0d3b3fc2bcb5dec70b13165efe1d169ee9e88282
yuka <>
2018-05-25 14:24:19 +0800

    First Commit

---
+++ sampleImageView/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  sampleImageView
+//
+//  Created by yuka on 2018/05/25.
+//  Copyright © 2018年 yuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        myImageView.image = UIImage(named: "nobita.jpg")
+    }
+
+    @IBAction func tapButton(_ sender: UIButton) {
+        myImageView.image = UIImage(named: "sample\(sender.tag).jpg")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
